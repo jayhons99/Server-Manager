be32413ef14e0604872b481ff427124d0663b7d8
Jhon Sisneros <jhonsisneros99@gmail.com>
2022-07-25 13:07:59 -0400

    Finished configuring the rest of API endpoints and configured database

--- src/main/java/com/jhonsisneros/server/ServerApplication.java
+++ src/main/java/com/jhonsisneros/server/ServerApplication.java
@@ -1,7 +1,12 @@
 package com.jhonsisneros.server;
 
+import com.jhonsisneros.server.enumeration.Status;
+import com.jhonsisneros.server.model.Server;
+import com.jhonsisneros.server.repository.ServerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -10,4 +15,45 @@ public class ServerApplication {
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner run(ServerRepository serverRepository) {
+		return args -> {
+			serverRepository.save(new Server(
+					null,
+					"192.168.1.160",
+					"Ubuntu",
+					"16 GB",
+					"PC",
+					"http://localhost:8080/server/image/server1.png",
+					Status.SERVER_UP)
+			);
+			serverRepository.save(new Server(
+					null,
+					"192.168.1.58",
+					"Kali",
+					"8 GB",
+					"Dell Tower",
+					"http://localhost:8080/server/image/server1.png",
+					Status.SERVER_DOWN)
+			);
+			serverRepository.save(new Server(
+					null,
+					"192.168.1.32",
+					"Fedora",
+					"12 GB",
+					"Web Server",
+					"http://localhost:8080/server/image/server1.png",
+					Status.SERVER_DOWN)
+			);
+			serverRepository.save(new Server(
+					null,
+					"192.168.1.121",
+					"MS 2008",
+					"32 GB",
+					"Mail Server",
+					"http://localhost:8080/server/image/server1.png",
+					Status.SERVER_UP)
+			);
+		};
+	}
 }
